247c386879529d87d409ca8899ea8552dc90a22a
srp1010 <srp1010@yahoo.co.uk>
2018-09-13 13:40:16 +0100

    Update index.js

--- functiona/index.js
+++ functiona/index.js
@@ -2,9 +2,9 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
-    currentTime.setTimezone("America/Los_Angeles");
+    currentTime.setTimezone("Asia/Shanghai");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Los Angeles is: ' + currentTime.toString(),
+        body: 'The time in Shanghai is: ' + currentTime.toString(),
     });
 };
